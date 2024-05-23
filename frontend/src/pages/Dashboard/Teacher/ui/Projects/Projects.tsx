ae072802567b948b90dba4a00750979dc2304c42
webency <webency@mail.ru>
2024-05-24 02:19:40 +0300

    add course view

--- frontend/src/pages/Dashboard/Teacher/ui/Projects/Projects.tsx
+++ frontend/src/pages/Dashboard/Teacher/ui/Projects/Projects.tsx
@@ -1,6 +1,7 @@
 import {cNames} from "@shared/lib/cNames/cNames.ts";
 
 import cls from './Projects.module.scss'
+import {useState} from "react";
 
 interface ProjectsProps {
     classname?: string;
@@ -8,11 +9,70 @@ interface ProjectsProps {
 
 const Projects = (props: ProjectsProps) => {
     const {classname = ""} = props;
+    const [courses] = useState(data)
     return (
         <div className={cNames(cls.Projects, {}, [classname])}>
-            Projects
+            <div className={cls.panel}>
+                <h1>Управление курсами</h1>
+            </div>
+            <div className={cls.courseWrapper}>
+                {courses && courses.map((course) => (
+                    <CourseCard
+                        key={course.id}
+                        title={course.title}
+                        language={course.language}
+                        lectureQty={course.lectureQty}
+                        testQty={course.testQty}
+                    />
+                ))}
+            </div>
+
         </div>
     )
 }
-
 export default Projects;
+
+
+interface CourseCardProps {
+    id?: number;
+    title: string;
+    language: string;
+    lectureQty: number;
+    testQty: number;
+}
+const CourseCard = (props:CourseCardProps) => {
+    const {testQty,lectureQty,language,title} = props;
+    return (
+        <div className={cls.CourseCard}>
+            <p>{language}</p>
+            <h2>{title}</h2>
+            <div className={cls.info}>
+                <span>Лекции: {lectureQty}</span>
+                <span>Тесты: {testQty}</span>
+            </div>
+        </div>
+    )
+}
+const data: CourseCardProps[] = [
+    {
+        id: 1,
+        title: 'Python Начало',
+        language: 'Python',
+        lectureQty: 10,
+        testQty: 5,
+    },
+    {
+        id: 2,
+        title: 'Объектно-ориентированное программирование',
+        language: 'Python',
+        lectureQty: 10,
+        testQty: 5,
+    },
+    {
+        id: 3,
+        title: 'Numpy & pandas',
+        language: 'Python',
+        lectureQty: 10,
+        testQty: 5,
+    }
+]
--- frontend/src/widgets/Dashboard/Teacher/Lecture/ui/AllLectures/AllLectures.tsx
+++ frontend/src/widgets/Dashboard/Teacher/Lecture/ui/AllLectures/AllLectures.tsx
@@ -2,6 +2,10 @@ import {cNames} from "@shared/lib/cNames/cNames.ts";
 
 import cls from './AllLectures.module.scss'
 import Card from "@widgets/Dashboard/Teacher/Card/Card.tsx";
+import {useState} from "react";
+import {Button, ButtonTheme} from "@shared/ui/Buttton/Button.tsx";
+import {CgRemoveR} from "react-icons/cg";
+import {FiEdit} from "react-icons/fi";
 
 interface AllLecturesProps {
     classname?: string;
@@ -9,13 +13,97 @@ interface AllLecturesProps {
 
 const AllLectures = (props: AllLecturesProps) => {
     const {classname = ""} = props;
+    const [lectues] = useState(data)
     return (
         <div className={cNames(cls.AllLectures, {}, [classname])}>
             <Card >
-                Все лекции
+                <table className={cls.table}>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Название</th>
+                            <th>Описание</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {lectues.map((lecture) => (
+                            <tr key={lecture.id}>
+                                <td>{lecture.id}</td>
+                                <td>{lecture.name}</td>
+                                <td>{lecture.description}</td>
+                                <td>
+                                    <Button theme={ButtonTheme.CLEAR}><FiEdit className={cls.editIcon} size={24}/></Button>
+                                    <Button theme={ButtonTheme.CLEAR}><CgRemoveR className={cls.deleteIcon} size={24} /></Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className={cls.pagination}>
+                    <div>Назад</div>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>Вперед</div>
+                </div>
             </Card>
         </div>
     )
 }
 
 export default AllLectures;
+
+const data = [
+    {
+        id: 1,
+        name: "Лекция 1",
+        description: "Описание лекции 1"
+    },
+    {
+        id: 2,
+        name: "Лекция 2",
+        description: "Описание лекции 2"
+    },
+    {
+        id: 3,
+        name: "Лекция 3",
+        description: "Описание лекции 3"
+    },
+    {
+        id: 4,
+        name: "Лекция 4",
+        description: "Описание лекции 4"
+    },
+    {
+        id: 5,
+        name: "Лекция 5",
+        description: "Описание лекции 5"
+    },
+    {
+        id: 6,
+        name: "Лекция 6",
+        description: "Описание лекции 6"
+    },
+    {
+        id: 7,
+        name: "Лекция 7",
+        description: "Описание лекции 7"
+    },
+    {
+        id: 8,
+        name: "Лекция 8",
+        description: "Описание лекции 8"
+    },
+    {
+        id: 9,
+        name: "Лекция 9",
+        description: "Описание лекции 9"
+    },
+    {
+        id: 10,
+        name: "Лекция 10",
+        description: "Описание лекции 10"
+    },
+
+]
