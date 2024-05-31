8685d862c8ca6ac9e257daaef05273bfef29f750
NadezhdaL2001 <nadyushka.likhanova2015@gmail.com>
2024-05-31 05:02:52 +0300

    Added new backend functionality

--- backend/src/main/java/ru/codeline/repositories/QuizRepository.java
+++ backend/src/main/java/ru/codeline/repositories/QuizRepository.java
@@ -1,10 +1,7 @@
 package ru.codeline.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.codeline.models.course.Lecture;
 import ru.codeline.models.course.Quiz;
-
-import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 }
